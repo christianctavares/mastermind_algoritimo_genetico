950ff87e7418b817d86a0819e6dae86eb464f555
christian.tavares <christian.tavares@al.infnet.edu.br>
2021-09-11 14:05:24 -0300

    organizando em funcoes separadas

--- algoritmoGenetico.py
+++ algoritmoGenetico.py
@@ -1,64 +1,95 @@
 from random import choices
 from typing import List
 from operator import itemgetter
-import random 
-
-TOGUESS = [random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6)]
-print()
-print("SENHA GERADA: ", TOGUESS)
+import random
 
 Genome = List[int]
 Population = List[Genome]
+
+# VARIABLE DECLARATION #
 WEIGHT_BLACK = 5 
 WEIGHT_WHITE = 3 
+CROSSOVER_PROBABILITY = 0.5
+MUTATION_PROBABILITY = 0.03
+TOGUESS = [random.randint(1, 6),random.randint(1, 6),random.randint(1, 6),random.randint(1, 6)]
 
 class bcolors:
     OKGREEN = '\033[92m'
     FAIL = '\033[91m'
 
-def robo():
-    def generate_genome(length: int) -> Genome:
-        return choices([1,2,3,4,5,6], k=length)
+def generate_genome(length: int) -> Genome:
+    return choices([1,2,3,4,5,6], k=length)
 
-    def generate_population(size: int, genome_lenght: int) -> Population:
-        return [generate_genome(genome_lenght) for _ in range(size)]
+def generate_population(size: int, genome_lenght: int) -> Population:
+    return [generate_genome(genome_lenght) for _ in range(size)]
 
-    def fitness(genome: Genome, guess: TOGUESS):
-        value = 0 
-        for i, j in zip(genome, guess):
+def fitness(genome: Genome, guess: TOGUESS):
+    value = 0 
+    for i, j in zip(genome, guess):
+        if i == j:
+            value += WEIGHT_BLACK
+        elif i in guess:
+            value += WEIGHT_WHITE
+    if value == 0:
+        return 0
+    return value
 
-                if i == j:
-                    value += WEIGHT_BLACK
-                elif i in guess:
-                    value += WEIGHT_WHITE
-        if value == 0:
-            return 0
-        return value
-    def ranked_func(solutions):
-        rankedsolutions = []
-        for s in solutions:
-            rankedsolutions.append([fitness(s, TOGUESS), s])
-        rankedsolutions = sorted(rankedsolutions, key=itemgetter(0), reverse=True)
-        return rankedsolutions
+def crossover(code1, code2):
+    newCode = []
+    for i in range(4):
+        if random.random() > CROSSOVER_PROBABILITY:
+            newCode.append(code1[i])
+        else:
+            newCode.append(code2[i])
+    return newCode
 
-    def cross_mutate(bestsolutions):
-        elements = []
-        for s in bestsolutions:
-            elements.append(s[1][0])
-            elements.append(s[1][1])
-            elements.append(s[1][2])
-            elements.append(s[1][3])
-        new_gen = []
-        #mudar popu
-        for j in range(60):
-            e_1 = random.choice(elements) #* random.uniform(0.99, 1.01)
-            e_2 = random.choice(elements) #* random.uniform(0.99, 1.01)
-            e_3 = random.choice(elements) #* random.uniform(0.99, 1.01)
-            e_4 = random.choice(elements) #* random.uniform(0.99, 1.01)
+def mutate(code):
+    position = random.randint(0, 3)
+    newValue = random.randint(1, 6)
+    code[position] = newValue
+    return code
 
-            new_gen.append([e_1,e_2,e_3,e_4])
-        return new_gen
-    
+def new_population(parents):
+    sons = []
+    for i in range(len(parents)):
+        if i == len(parents) - 1:
+            sons.append(parents[i])
+            break
+        son = crossover(parents[i], parents[i+1])
+        if random.random() <= MUTATION_PROBABILITY:
+            son = mutate(son)     
+        sons.append(son)
+    return sons
+
+def ranked_func(solutions):
+    rankedsolutions = []
+    for s in solutions:
+        rankedsolutions.append([fitness(s, TOGUESS), s])
+    rankedsolutions = sorted(rankedsolutions, key=itemgetter(0), reverse=True)
+    return rankedsolutions
+
+def cross_mutate(bestsolutions):
+    elements = []
+    for s in bestsolutions:
+        elements.append(s[1][0])
+        elements.append(s[1][1])
+        elements.append(s[1][2])
+        elements.append(s[1][3])
+    new_gen = []
+    for j in range(60):
+        e_1 = random.choice(elements)
+        e_2 = random.choice(elements)
+        e_3 = random.choice(elements)
+        e_4 = random.choice(elements)
+        new_gen.append([e_1,e_2,e_3,e_4])
+    #print("New Gen 1", new_gen)
+    if random.random() <= MUTATION_PROBABILITY:
+        m = random.random
+        new_gen[m] = mutate(new_gen[m])
+        #print("New Gen 2", new_gen)
+    return new_gen
+
+def ai():
     popu = generate_population(60, 4)
     solutions = []
     for s in popu:
@@ -67,28 +98,19 @@ def robo():
     for i in popu:
         count += 1
         rankedsolutions = ranked_func(solutions)
-        #print(f"--- Genoma {i[0]} best solution === ")
-        print(count, "RANKEDSOLITIOM: " ,rankedsolutions)
-        #mudar popu
         bestsolutions = rankedsolutions[:19]
         solutions = cross_mutate(bestsolutions)
-        
         rankedsolutions = ranked_func(solutions)
         count += 1
-        print(count, "RANKEDSOLITIOM: " ,rankedsolutions)
+        if rankedsolutions[0][0] >=16:
+            return rankedsolutions[0][1] 
 
-        if rankedsolutions[0][0] >16:
-            tentativa = rankedsolutions[0][1]
-            # print("TENTATIVA: ", tentativa)
-            # print("SENHA CORRETA: ",TOGUESS)
-            return tentativa
-
-def game(tentativa):
+def game(attempt):
     chances = 8
-    jogada = 1
-    if tentativa == TOGUESS:
+    move = 1
+    if attempt == TOGUESS:
         print()
-        print(f"{jogada}° Jogada realizada: {tentativa}")
+        print(f"{move}° Jogada realizada: {attempt}")
         print()
         print(bcolors.OKGREEN + "█░█ █ ▀█▀ █▀█ █▀█ █ ▄▀█ █")
         print(bcolors.OKGREEN + "▀▄▀ █ ░█░ █▄█ █▀▄ █ █▀█ ▄")
@@ -96,9 +118,8 @@ def game(tentativa):
         print("==== Você acertou de PRIMEIRA! Parabens ====")
         print()
     else:
-        rodada = 0
-
-        while tentativa != TOGUESS:
+        round = 0
+        while attempt != TOGUESS or chances == 0:
             if chances == 0:
                 print()
                 print(bcolors.FAIL + "█▀▀ ▄▀█ █▀▄▀█ █▀▀   █▀█ █░█ █▀▀ █▀█")
@@ -106,50 +127,48 @@ def game(tentativa):
                 print()
                 print("Acabou suas chances de acertar (╯°□°）╯︵ ┻━┻")            
                 return
-            rodada += 1
+            round += 1
             count =  0
 
-            acertos = ['X','X','X','X']
+            hits = ['X','X','X','X']
 
             for i in range(0, 4):
-                
-                if(tentativa[i] == TOGUESS[i]):
+                if(attempt[i] == TOGUESS[i]):
                     count += 1
-                    acertos[i] = tentativa[i]
+                    hits[i] = attempt[i]
                 else:
                     continue
-            
+
             if count < 4 and count != 0:  
-                print(f"{jogada}° Jogada realizada: {tentativa}")
+                print(f"{move}° Jogada realizada: {attempt}")
                 print("Você não achou a sequência, mas encontrou", count, "digito(s) corretos")
                 print("Números corretos:")
-                for k in acertos:
+                for k in hits:
                     print(k, end=' ')
                 print('\n')
                 chances -= 1
-                jogada += 1
-                tentativa = robo()
+                move += 1
+                attempt = ai()
             elif count == 0:
-                print(f"{jogada}° Jogada realizada: {tentativa}")
+                print(f"{move}° Jogada realizada: {attempt}")
                 print("Nenhum dos números escolhidos eram corretos")
+                print('\n')
                 chances -= 1
-                jogada += 1
-                tentativa = robo()
+                move += 1
+                attempt = ai()
 
-        if tentativa == TOGUESS:  
-            rodada += 1
+        if attempt == TOGUESS:  
+            round += 1
             chances -= 1
-            print(f"{jogada}° Jogada realizada: {tentativa}")
+            print(f"{move}° Jogada realizada: {attempt}")
             print("Você achou a sequência de digitos corretos!")
             print()
             print(bcolors.OKGREEN + "█░█ █ ▀█▀ █▀█ █▀█ █ ▄▀█ █")
             print(bcolors.OKGREEN + "▀▄▀ █ ░█░ █▄█ █▀▄ █ █▀█ ▄")
             print()
-            print("==== Você acertou em",rodada,"tentativa ====")
+            print("==== Você acertou em",round,"tentativas ====")
             print()
 
-def jogar():
-    tentativa = robo()
-    game(tentativa)
-
-jogar()
+# MAIN #
+print(f"\nSENHA GERADA: {TOGUESS}\n")
+game(ai())
