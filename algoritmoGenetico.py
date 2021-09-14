3a45e3ec171f93eb76bda78d3805e44aab233b6c
christian.tavares <christian.tavares@al.infnet.edu.br>
2021-09-13 23:11:28 -0300

    projeto finalizado

--- algoritmoGenetico.py
+++ algoritmoGenetico.py
@@ -1,9 +1,7 @@
 from random import choices
 from typing import List
 from operator import itemgetter
-import sys
 import random
-import numpy as np
 
 Genome = List[int]
 Population = List[Genome]
@@ -11,18 +9,19 @@ Population = List[Genome]
 # VARIABLE DECLARATION #
 WEIGHT_BLACK = 5 
 WEIGHT_WHITE = 3 
-<<<<<<< HEAD
 CROSSOVER_PROBABILITY = 0.5
 MUTATION_PROBABILITY = 0.03
 TOGUESS = []
-=======
->>>>>>> 9accd38 (mais viadagem)
 
-class bcolors:
+SELECTED_SOLUTIONS = 20
+PONTUACAO_FITNESS_ACEITAVEL = 16
+NUMERO_DA_POPULACAO = 60
+TAMANHO_DO_GENOMA = 4
+
+class ConsoleCores:
     OKGREEN = '\033[92m'
     FAIL = '\033[91m'
 
-<<<<<<< HEAD
 def generate_genome(length: int) -> Genome:
     return choices([1,2,3,4,5,6], k=length)
 
@@ -40,15 +39,19 @@ def fitness(genome: Genome, guess: TOGUESS):
         return 0
     return value
 
-def crossover(code1, code2, position):
-    new_code1 = np.append([code1[:position], code2[position:]])
-    new_code2 = np.append([code2[:position], code1[position:]])
-    return new_code1, new_code2
+def crossover(code1, code2):
+    new_code = []
+    for genoma in range(TAMANHO_DO_GENOMA):
+        if random.random() > CROSSOVER_PROBABILITY:
+            new_code.append(code1[genoma])
+        else:
+            new_code.append(code2[genoma])
+    return new_code
 
 def mutate(code):
     position = random.randint(0, 3)
-    newValue = random.randint(1, 6)
-    code[position] = newValue
+    new_value = random.randint(1, 6)
+    code[position] = new_value
     return code
 
 def new_population(parents):
@@ -65,86 +68,39 @@ def new_population(parents):
 
 def ranked_func(solutions):
     rankedsolutions = []
-    for s in solutions:
-        rankedsolutions.append([fitness(s, TOGUESS), s])
+    for solution in solutions:
+        rankedsolutions.append([fitness(solution, TOGUESS), solution])
     rankedsolutions = sorted(rankedsolutions, key=itemgetter(0), reverse=True)
     return rankedsolutions
 
-def cross_mutate(bestsolutions):
+def cross_mutate(best_solutions):
     elements = []
-    for s in bestsolutions:
-        elements.append(s[1][0])
-        elements.append(s[1][1])
-        elements.append(s[1][2])
-        elements.append(s[1][3])
+    for solution in best_solutions:
+        elements.append(solution[1][0])
+        elements.append(solution[1][1])
+        elements.append(solution[1][2])
+        elements.append(solution[1][3])
     new_gen = []
-    for j in range(60):
+    for j in range(NUMERO_DA_POPULACAO):
         e_1 = random.choice(elements)
         e_2 = random.choice(elements)
         e_3 = random.choice(elements)
         e_4 = random.choice(elements)
         new_gen.append([e_1,e_2,e_3,e_4])
-    #print("New Gen 1", new_gen)
-    if random.random() <= MUTATION_PROBABILITY:
-        m = random.random
-        new_gen[m] = mutate(new_gen[m])
-        #print("New Gen 2", new_gen)
+
     return new_gen
 
 def ai():
-=======
-def robo():
-    def generate_genome(length: int) -> Genome:
-        return choices([1,2,3,4,5,6], k=length)
+    popu = generate_population(NUMERO_DA_POPULACAO, TAMANHO_DO_GENOMA)
+    solutions = popu
 
-    def generate_population(size: int, genome_lenght: int) -> Population:
-        return [generate_genome(genome_lenght) for _ in range(size)]
-
-    def fitness(genome: Genome, guess: TOGUESS):
-        value = 0 
-        for i, j in zip(genome, guess):
-
-                if i == j:
-                    value += WEIGHT_BLACK
-                elif i in guess:
-                    value += WEIGHT_WHITE
-        if value == 0:
-            return 0
-        return value
-
-    def cross_mutate(bestsolutions):
-        elements = []
-        for s in bestsolutions:
-            elements.append(s[1][0])
-            elements.append(s[1][1])
-            elements.append(s[1][2])
-            elements.append(s[1][3])
-        new_gen = []
-        #mudar popu
-        for j in range(60):
-            e_1 = random.choice(elements) #* random.uniform(0.99, 1.01)
-            e_2 = random.choice(elements) #* random.uniform(0.99, 1.01)
-            e_3 = random.choice(elements) #* random.uniform(0.99, 1.01)
-            e_4 = random.choice(elements) #* random.uniform(0.99, 1.01)
-
-            new_gen.append([e_1,e_2,e_3,e_4])
-        return new_gen
-    
->>>>>>> 9accd38 (mais viadagem)
-    popu = generate_population(60, 4)
-    solutions = []
-    for s in popu:
-        solutions.append(s)
-    count = 0
     for i in popu:
-        count += 1
         rankedsolutions = ranked_func(solutions)
-        bestsolutions = rankedsolutions[:19]
-        solutions = cross_mutate(bestsolutions)
+        best_solutions = rankedsolutions[:SELECTED_SOLUTIONS]
+        solutions = cross_mutate(best_solutions)
         rankedsolutions = ranked_func(solutions)
-        count += 1
-        if rankedsolutions[0][0] >=16:
-            return rankedsolutions[0][1] 
+        if rankedsolutions[0][0] >= PONTUACAO_FITNESS_ACEITAVEL:
+            return rankedsolutions[0][1]
 
 def game(attempt):
     chances = 8
@@ -153,8 +109,8 @@ def game(attempt):
         print()
         print(f"{move}° Jogada realizada: {attempt}")
         print()
-        print(bcolors.OKGREEN + "█░█ █ ▀█▀ █▀█ █▀█ █ ▄▀█ █")
-        print(bcolors.OKGREEN + "▀▄▀ █ ░█░ █▄█ █▀▄ █ █▀█ ▄")
+        print(ConsoleCores.OKGREEN + "█░█ █ ▀█▀ █▀█ █▀█ █ ▄▀█ █")
+        print(ConsoleCores.OKGREEN + "▀▄▀ █ ░█░ █▄█ █▀▄ █ █▀█ ▄")
         print()
         print("==== Você acertou de PRIMEIRA! Parabens ====")
         print()
@@ -163,8 +119,8 @@ def game(attempt):
         while attempt != TOGUESS or chances == 0:
             if chances == 0:
                 print()
-                print(bcolors.FAIL + "█▀▀ ▄▀█ █▀▄▀█ █▀▀   █▀█ █░█ █▀▀ █▀█")
-                print(bcolors.FAIL + "█▄█ █▀█ █░▀░█ ██▄   █▄█ ▀▄▀ ██▄ █▀▄")
+                print(ConsoleCores.FAIL + "█▀▀ ▄▀█ █▀▄▀█ █▀▀   █▀█ █░█ █▀▀ █▀█")
+                print(ConsoleCores.FAIL + "█▄█ █▀█ █░▀░█ ██▄   █▄█ ▀▄▀ ██▄ █▀▄")
                 print()
                 print("Acabou suas chances de acertar (╯°□°）╯︵ ┻━┻")            
                 return
@@ -173,10 +129,10 @@ def game(attempt):
 
             hits = ['X','X','X','X']
 
-            for i in range(0, 4):
-                if(attempt[i] == TOGUESS[i]):
+            for peca in range(0, 4):
+                if(attempt[peca] == TOGUESS[peca]):
                     count += 1
-                    hits[i] = attempt[i]
+                    hits[peca] = attempt[peca]
                 else:
                     continue
 
@@ -184,8 +140,8 @@ def game(attempt):
                 print(f"{move}° Jogada realizada: {attempt}")
                 print("Você não achou a sequência, mas encontrou", count, "digito(s) corretos")
                 print("Números corretos:")
-                for k in hits:
-                    print(k, end=' ')
+                for numeros in hits:
+                    print(numeros, end=' ')
                 print('\n')
                 chances -= 1
                 move += 1
@@ -204,8 +160,8 @@ def game(attempt):
             print(f"{move}° Jogada realizada: {attempt}")
             print("Você achou a sequência de digitos corretos!")
             print()
-            print(bcolors.OKGREEN + "█░█ █ ▀█▀ █▀█ █▀█ █ ▄▀█ █")
-            print(bcolors.OKGREEN + "▀▄▀ █ ░█░ █▄█ █▀▄ █ █▀█ ▄")
+            print(ConsoleCores.OKGREEN + "█░█ █ ▀█▀ █▀█ █▀█ █ ▄▀█ █")
+            print(ConsoleCores.OKGREEN + "▀▄▀ █ ░█░ █▄█ █▀▄ █ █▀█ ▄")
             print()
             print("==== Você acertou em",round,"tentativas ====")
             print()
@@ -213,11 +169,11 @@ def game(attempt):
 # MAIN #
 senha = []
 
-for i in range(1, 5):
-    valor = input(f'\nInsira o {i}º valor da senha: ')
-    while (valor.isnumeric() != True or int(valor) < 0 or int(valor) > 6):
+for posicao in range(1, 5):
+    valor = input(f'\nInsira o {posicao}º valor da senha: ')
+    while (valor.isnumeric() is not True or int(valor) < 0 or int(valor) > 6):
         print("O valor inserido deve ser númerico e entre 0 e 6\n")
-        valor = input(f'\nInsira novamente o {i}º valor da senha: ')
+        valor = input(f'\nInsira novamente o {posicao}º valor da senha: ')
     senha.append(int(valor))
 TOGUESS = senha
 print(f"\nSENHA escolhida: {TOGUESS}\n")
